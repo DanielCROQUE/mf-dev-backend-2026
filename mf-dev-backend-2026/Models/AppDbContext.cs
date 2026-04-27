cb25c039b41127bef22b1848a038f9f653696be0
DanielCROQUE <https://github.com/DanielCROQUE>
2026-04-26 21:37:28 -0300

    configuração do Banco de dados e da tabela veiculo

---
+++ mf-dev-backend-2026/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_dev_backend_2026.Models
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Veículo> Veiculos { get; set; }
+    }
+}
